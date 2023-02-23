9ee2d8913adc079197002fd7611db23318d3a62e
kaipis <kaipis@DESKTOP-DP5RFOO.localdomain>
2023-02-23 16:49:42 +0200

    winners

--- frontend/src/Views/EventSelectionPage/EventSelectionPage.jsx
+++ frontend/src/Views/EventSelectionPage/EventSelectionPage.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./EventSelectionPage.css";
 
-const EventSelectionPage = async () => {
+const EventSelectionPage = () => {
   // fetch call for geting the list of events
   // fetch here
+  const [eestecEventList, setData] = useState([]);
 
-  const res = await fetch("http://localhost:8000/api/events").then((data) =>
-    data.json()
-  );
-  console.log(res);
+  async function load() {
+    await fetch("http://localhost:8000/api/events")
+      .then((data) => data.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }
 
-  const eestecEventList = [
+  load();
+
+  /*
+  setData(
+    [
     { id: 0, name: "event 1" },
     { id: 1, name: "event 2" },
     { id: 2, name: "event 3" },
-  ];
+  ]);
+*/
 
   function deleteEvent(eestecEventId) {
     // make fetch request for deleting an event of id of eestecEventId, by user of id: this users id
@@ -25,12 +36,12 @@ const EventSelectionPage = async () => {
       <h4>EventSelectionPage</h4>
       <ul className="event-list">
         {eestecEventList.map((eestecEvent, index) => (
-          <div>
-            <li key={eestecEvent.id}>{eestecEvent.name}</li>
+          <div className="rotate">
+            <li key={eestecEvent.id}>{eestecEvent.name} </li>
 
             <Link to={"/events/" + eestecEvent.id}>Evaluate Applications</Link>
 
-            <button onClick={() => deleteEvent(eestecEvent.id)}>
+            <button onClick={() => deleteEvent(eestecEvent.id)} className="but">
               Delete Event
             </button>
           </div>
